c09fcbcc28d4dacda1a35961bc55e6376adf7056
aaron pedwell <aaron.pedwell@1e.com>
2023-10-04 19:25:25 +0100

    #19 - add header component with github link

---
+++ src/components/header/header.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+import githubLogo from "./github.svg";
+
+const StyledHeader = styled.header`
+  display: flex;
+  align-items: center;
+  background-color: #02b3af;
+  color: white;
+  justify-content: center;
+
+  h1 {
+    background-color: #02b3af;
+  }
+`;
+
+const StyledLink = styled.a`
+  margin-left: 20px;
+  border-radius: 50%;
+
+  img {
+    border-radius: 50%;
+  }
+`;
+
+export const Header = () => {
+  return (
+    <>
+      <StyledHeader>
+        <h1>Hacktoberfest 2023 Memes Collection</h1>
+        <StyledLink
+          href="https://github.com/Alexandra-Haynes/hacktoberfest2023-memes-collection"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={githubLogo} alt="github repository" width="30px" />
+        </StyledLink>
+      </StyledHeader>
+    </>
+  );
+};
